853f4b213135851026a94cb1ddba6d712d21ae70
Ion Stici <ionsticidev@gmail.com>
2023-01-15 14:15:41 +0200

    Add tip calculator function

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,29 @@
 "use strict";
 
+// const billMessage = document.querySelector(".calc__bill-message");
+// billMessage.style.opacity = "1";
+// billInput.style.border = "2px solid #E17052";
+
+// const numMessage = document.querySelector(".calc__num-message");
+// numMessage.style.opacity = "1";
+// numPeopleInput.style.border = "2px solid #E17052";
+// tipInput.style.border = "2px solid #E17052";
+
+// SELECT ELEMENTS
+
 const billInput = document.querySelector("#calc__bill__input");
 const tipInput = document.querySelector(".calc__tip-input");
 const tipBtns = document.querySelectorAll(".calc__tip-btn");
 const numPeopleInput = document.querySelector("#calc__num-input");
 
+const form = document.querySelector(".calc__col-1");
+
 const tipResult = document.querySelector(".calc__result-tip");
 const totalResult = document.querySelector(".calc__result-total");
 
 const btn = document.querySelector(".calc__result-btn");
+
+// RESET
 
 btn.addEventListener("click", function () {
     if (btn.classList.contains("calc__result-btn--reset")) return;
@@ -28,17 +43,65 @@ btn.addEventListener("click", function () {
     });
 });
 
-if (tipResult.textContent !== "$0.00" && totalResult.textContent !== "$0.00")
-    btn.classList.remove("calc__result-btn--reset");
+// VALIDATE
 
-const billMessage = document.querySelector(".calc__bill-message");
-// billMessage.style.opacity = "1";
+const calcTip = function (bill, tip, people) {
+    if (!(bill && bill > 0)) return;
+    if (!(tip && tip > 0 && tip <= 100)) return;
+    if (!(people && people > 0)) return;
 
-// billInput.style.border = "2px solid #E17052";
+    const tipPerPerson = ((bill * (tip / 100)) / people).toFixed(2);
+    const totalPerPerson = (bill / people).toFixed(2);
 
-const numMessage = document.querySelector(".calc__num-message");
-// numMessage.style.opacity = "1";
+    tipResult.textContent = tipPerPerson;
+    totalResult.textContent = totalPerPerson;
 
-// numPeopleInput.style.border = "2px solid #E17052";
+    // prettier-ignore
+    if (tipResult.textContent !== "$0.00" &&totalResult.textContent !== "$0.00")
+        btn.classList.remove("calc__result-btn--reset");
+};
 
-// tipInput.style.border = "2px solid #E17052";
+// SET ACTIVE BUTTONS
+
+tipBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+        tipBtns.forEach((btn) => {
+            btn.classList.remove("calc__tip-btn--active");
+        });
+
+        btn.classList.add("calc__tip-btn--active");
+
+        calcTip(
+            billInput.value,
+            Number.parseInt(btn.textContent, 10),
+            numPeopleInput.value
+        );
+    });
+});
+
+// SUBMIT
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    calcTip(billInput.value, tipInput.value, numPeopleInput.value);
+});
+
+billInput.addEventListener("focusout", function () {
+    calcTip(billInput.value, tipInput.value, numPeopleInput.value);
+});
+
+tipInput.addEventListener("focusout", function () {
+    let tip;
+
+    tipBtns.forEach((btn) => {
+        if (btn.classList.contains("calc__tip-btn--active")) {
+            tip = Number.parseInt(btn.textContent, 10);
+            calcTip(billInput.value, tip, numPeopleInput.value);
+        }
+    });
+
+    if (!tip) calcTip(billInput.value, tipInput.value, numPeopleInput.value);
+});
+
+numPeopleInput.addEventListener("focusout", function () {
+    calcTip(billInput.value, tipInput.value, numPeopleInput.value);
+});
